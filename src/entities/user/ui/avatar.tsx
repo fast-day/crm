888543be9af87a17b2b681c05aaacb392d0c8602
Xaletch <kiril.kolesnikov0609@mail.ru>
2026-06-14 15:41:19 +0300

    bug fixed

--- src/entities/user/ui/avatar.tsx
+++ src/entities/user/ui/avatar.tsx
@@ -54,7 +54,7 @@ export const Avatar = ({ id, avatar_url, size, name, className, opacity, childre
       ) : (
         isIcon ? <>{icon}</> :
         <span className="uppercase font-medium leading-5 select-none tracking-wider">
-          {name.trim().includes(' ') ? (name.trim()[0] + name.trim().split(/\s*[—–-]\s*/)[1][0]).toUpperCase() : name.trim()[0].toUpperCase()}
+          {name.trim().includes(' ') ? (name.trim()[0] + name.trim().split(/\s*[—–-]\s*/).map(w => w[0]).join("")).toUpperCase() : name.trim()[0].toUpperCase()}
         </span>
       )}
       {}
